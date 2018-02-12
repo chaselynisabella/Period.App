a1078293301810635aa5f4822d6defcf4d7130a0
chaselynisabella <chaselynisabella@gmail.com>
2018-02-12 01:25:36 -0500

    Add files via upload

---
+++ HelloWorld/Period/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Chaselyn Baca on 2/10/18.
+//  Copyright © 2018 Chaselyn Baca. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController:
+//MARK: Properties
+UIViewController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    
+
+}
+
